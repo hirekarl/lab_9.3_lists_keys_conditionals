2b389f915cc639569a5a3aa64357d02ce576dcf0
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-26 21:24:14 -0400

    Fixed root-level logic (fixed filtering problems).

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -1,13 +1,23 @@
 import { useState, type ReactNode } from "react"
-import type { Task, TaskStatus } from "./types"
+import type {
+  Task,
+  TaskID,
+  TaskPriorityFilter,
+  TaskStatus,
+  TaskStatusFilter,
+} from "./types"
+import TaskFilter from "./components/TaskFilter/TaskFilter"
 import TaskList from "./components/TaskList/TaskList"
 
 import { MOCK_TASKS } from "./data/mockTasks"
 
 function App(): ReactNode {
   const [tasks, setTasks] = useState<Task[]>(MOCK_TASKS)
+  const [visibleTaskIds, setVisibleTaskIds] = useState<TaskID[]>(
+    tasks.map((t) => t.id)
+  )
 
-  function handleStatusChange(taskId: string, newStatus: TaskStatus): void {
+  function handleStatusChange(taskId: TaskID, newStatus: TaskStatus): void {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
         task.id === taskId ? { ...task, status: newStatus } : task
@@ -15,20 +25,36 @@ function App(): ReactNode {
     )
   }
 
-  function handleDelete(taskId: string): void {
+  function handleDelete(taskId: TaskID): void {
     setTasks(tasks.filter((t) => t.id !== taskId))
   }
 
+  function handleFilter(filters: {
+    status?: TaskStatusFilter
+    priority?: TaskPriorityFilter
+  }): void {
+    if ("status" in filters && filters.status !== "all") {
+      setVisibleTaskIds(
+        tasks.filter((t) => t.status === filters.status).map((t) => t.id)
+      )
+    } else if ("priority" in filters && filters.priority !== "all") {
+      setVisibleTaskIds(
+        tasks.filter((t) => t.priority === filters.priority).map((t) => t.id)
+      )
+    } else {
+      setVisibleTaskIds(tasks.map((t) => t.id))
+    }
+  }
+
   return (
-    <>
-      <div className="container">
-        <TaskList
-          tasks={tasks}
-          onStatusChange={handleStatusChange}
-          onDelete={handleDelete}
-        />
-      </div>
-    </>
+    <div className="container">
+      <TaskFilter onFilterChange={handleFilter} />
+      <TaskList
+        tasks={tasks.filter((t) => visibleTaskIds.includes(t.id))}
+        onStatusChange={handleStatusChange}
+        onDelete={handleDelete}
+      />
+    </div>
   )
 }
 
