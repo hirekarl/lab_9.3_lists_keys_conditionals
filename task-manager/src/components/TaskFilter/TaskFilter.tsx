4aba45fda8ca0024c557cbaad2c7fe2073cddfd4
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-26 21:23:51 -0400

    Fixed TaskFilter.

--- task-manager/src/components/TaskFilter/TaskFilter.tsx
+++ task-manager/src/components/TaskFilter/TaskFilter.tsx
@@ -1,5 +1,59 @@
-import type { ReactNode } from "react"
+import type { ReactNode, ChangeEvent } from "react"
+import type {
+  TaskFilterProps,
+  TaskPriorityFilter,
+  TaskStatusFilter,
+} from "../../types"
 
-export default function TaskFilter(): ReactNode {
-  return <div>TaskFilter</div>
+export default function TaskFilter({
+  onFilterChange,
+}: TaskFilterProps): ReactNode {
+  function handlePrioritySelect(event: ChangeEvent<HTMLSelectElement>): void {
+    if (
+      event.target.value === "low" ||
+      event.target.value === "medium" ||
+      event.target.value === "high" ||
+      event.target.value === "all"
+    ) {
+      const newPriority: TaskPriorityFilter = event.target.value
+      onFilterChange({ priority: newPriority })
+    }
+  }
+
+  function handleStatusSelect(event: ChangeEvent<HTMLSelectElement>): void {
+    if (
+      event.target.value === "pending" ||
+      event.target.value === "in-progress" ||
+      event.target.value === "completed" ||
+      event.target.value === "all"
+    ) {
+      const newStatus: TaskStatusFilter = event.target.value
+      onFilterChange({ status: newStatus })
+    }
+  }
+
+  return (
+    <div className="row">
+      <div className="col-md-6 offset-md-3 col-sm-10 offset-sm-1 mb-3">
+        <div className="d-flex justify-content-between">
+          <div>
+            <select onChange={handlePrioritySelect} className="form-select">
+              <option value="all">Filter by Priority</option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+          <div>
+            <select onChange={handleStatusSelect} className="form-select">
+              <option value="all">Filter by Status</option>
+              <option value="pending">Pending</option>
+              <option value="in-progress">In Progress</option>
+              <option value="completed">Completed</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
